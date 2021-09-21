3a20deac2bb2ae30690dbcde414d49a6b33be5b8
janbancarewicz <jan.bancarewicz@gmail.com>
2021-09-21 15:57:46 +0200

    react fron app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-const  App = () => {
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen'
+
+const App = () => {
   return (
-    <>
-      <h1>Welcome in ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/products/:id' component={ProductScreen}/>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
- 
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,21 @@
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+
+      <Container >
+        <Row>
+          <Col className='text-center py-3'>
+            Copyrt 
+          </Col>
+        </Row>
+        
+        
+         </Container>
+      
+    </footer>
+  )
+}
+
+export default Footer
